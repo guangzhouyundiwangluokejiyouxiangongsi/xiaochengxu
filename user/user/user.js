c9111e87718be358fbd31991323e91c178e36a27
guangzhouyundiwangluokejiyouxiangongsi <vzhoufei@163.com>
2017-07-15 09:45:06 +0800

    sss

---
+++ user/user/user.js
@@ -0,0 +1,27 @@
+// user.js
+Page({
+  data: {
+    content: [],
+    qita: [],
+    nzopen: ''
+  },
+  onLoad: function () {
+    this.setData({
+      qita: ['云商会', '我的云谱', '微微', '我的云'],
+    })
+  },
+  // 其他
+  list: function (e) {
+    if (this.data.nzopen) {
+      this.setData({
+        nzopen: false
+      })
+    } else {
+      this.setData({
+        content: this.data.qita,
+        nzopen: true
+      })
+    }
+  },
+
+})
